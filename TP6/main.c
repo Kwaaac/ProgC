b47535255d05b6b5724c0d0d621ec16df17f1f9c
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2020-12-04 12:37:54 +0100

    minor changes - Test nouvelle Branch
    Petites modifications pour tester la nouvelle branche

--- TP6/main.c
+++ TP6/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "stack_height.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    int i;
+
+    if (argc != 2) {
+        fprintf(stderr, "Nope, I need one positive integer in argument to work properly.\n");
+    }
+
+    stack_adr(atoi(argv[1]), &i, &i);
+
     return 0;
 }
--- TP6/stack_height.c
+++ TP6/stack_height.c
@@ -36,18 +36,3 @@ int stack_adr(int n, int *height, int *start_adr) {
     }
     return p;
 }
-
-/* main example
-
-int main(int argc, char* argv[]){
-    int i;
-
-    if (argc != 2){
-        fprintf(stderr, "Nope, I need one positive integer in argument to work properly.\n");
-    }
-
-    stack_adr(atoi(argv[1]), &i, &i);
-
-    return 0;
-}
- */
