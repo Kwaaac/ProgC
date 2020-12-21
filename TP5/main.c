81ca992d3a2edda5e06fb73aa5f45ce26858964f
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2020-12-21 19:01:56 +0100

    TP5 - Changements suite à la review de Guillaume

--- TP5/main.c
+++ TP5/main.c
@@ -1,17 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "array.h"
 
+void test_create_negative_and_empty_array() {
+    int *array;
 
-void test_concat_array(){
-    printf("\n\n========= TEST CONCAT ARRAY========= \n\n");
-    int* array1;
-    int* array2;
+    printf("\n\n========= TEST NEGATIVE ARRAY ========= \n\n");
+
+    array = allocate_integer_array(-5);
+
+    if (array == NULL) {
+        printf("Exception relevée");
+    }
+
+    array = allocate_integer_array(0);
+}
+
+void test_fill_array() {
+    int *array;
+
+    printf("\n\n========= TEST FILL ARRAY ========= \n\n");
+
+    array = fill_array();
+
+    printf("Voici l'array que vous venez de créer: ");
+    print_array(array);
+    free_integer_array(array);
+}
+
+/**
+ * Test method for equals arrays
+ */
+void test_arrays_equals() {
+    int *array1;
+    int *array2;
+
+    printf("Donnez manuellement deux tableaux afin de comparer si ils sont égaux ou non.\n");
+
+    array1 = fill_array();
+    array2 = fill_array();
+
+    printf("%s", are_arrays_equal(array1, array1) ? "Les tableaux sont egaux\n" : "Les tableaux ne sont pas égaux\n");
+
+    free_integer_array(array1);
+    free_integer_array(array2);
+}
+
+/**
+ * Test method for an copied array
+ */
+void test_copy_array() {
+    int *array;
+    int *copy;
+    int i;
+
+    printf("\n\n========= TEST COPY ARRAY ========= \n\n");
+
+    for (i = 0; i < 5; i++) {
+        array = random_array(10, 50);
+        copy = copy_array(array);
+
+        printf("\narray: ");
+        print_array(array);
+
+        printf("\ncopy: ");
+        print_array(copy);
+        printf("\n");
+
+        printf("%s", are_arrays_equal(array, copy) ? "Les tableaux sont egaux\n" : "Les tableaux ne sont pas égaux\n");
+    }
+
+    free_integer_array(array);
+    free_integer_array(copy);
+}
+
+/*
+ * Test method for the concatenations
+ */
+void test_concat_array() {
+    int *array1;
+    int *array2;
+
+    printf("\n\n========= TEST CONCAT ARRAY ========= \n\n");
 
     array1 = random_array(5, 20);
     array2 = random_array(8, 15);
-
 
     printf("array1: ");
     print_array(array1);
@@ -26,20 +101,26 @@ void test_concat_array(){
     free_integer_array(array2);
 }
 
-void test_merge_sort(){
-    printf("\n\n========= TEST MERGE SORT========= \n\n");
-
+/**
+ * Test method of the merge sort
+ */
+void test_merge_sort() {
     int *res;
+
+    printf("\n\n========= TEST MERGE SORT ========= \n\n");
 
     res = random_array(20, 100);
 
+
     print_array(res);
+
+    printf("Size of the array %d\n", array_size(res));
 
     res = merge_sort(res);
 
     printf(" \n\n");
     print_array(res);
-    printf("%d", array_size(res));
+    printf("Size of the final array %d\n", array_size(res));
 
     free_integer_array(res);
 }
@@ -49,9 +130,21 @@ void test_merge_sort(){
 int main(int argc, char *argv[]) {
     srand(time(NULL));
 
+    printf("=============================");
+    printf("\n========= TP5 TESTS =========\n");
+    printf("=============================");
+
+    test_create_negative_and_empty_array();
+
+    test_copy_array();
+
     test_concat_array();
 
     test_merge_sort();
+
+    if (argc > 1 && strcmp(argv[1], "-u") == 0) {
+        test_arrays_equals();
+    }
 
     return 0;
 }
