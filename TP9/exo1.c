de58783858705283cfa2fa6937fdbc7171d135cd
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2021-01-16 21:05:03 +0100

    TP9 - Finished

--- TP9/exo1.c
+++ TP9/exo1.c
@@ -48,14 +48,35 @@ void print_array(int *array, int size) {
     }
 }
 
+void print_info_zone(void *adr) {
+    size_t* info_adr = (size_t *)adr;
+
+    printf("Zone a l'adresse hexa : %p\n", adr);
+
+    printf("%lu \n", info_adr[-1]);
+
+    printf("%lu \n", info_adr[-2]);
+}
+
 int main(int argc, char *argv[]) {
     int size = atoi(argv[1]);
     int *tab = allocate_array(size);
+    int *tab2 = allocate_array(size + 5);
 
     fill_array(tab, size);
+    fill_array(tab2, size + 5);
 
     print_array(tab, size);
+    print_array(tab2, size + 5);
+
+    print_info_zone(tab);
+    print_info_zone(tab2);
 
     free(tab);
+    print_info_zone(tab2);
+
+    print_info_zone(tab);
+    print_info_zone(tab2);
+
     return 0;
 }
--- TP9/main.c
+++ TP9/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+void print_info_zone(void *adr) {
+    size_t* info_adr = (size_t *)adr;
+
+    printf("Zone a l'adresse hexa : %p\n", adr);
+
+    printf("%lu \n", info_adr[-1]);
+
+    printf("%lu \n", info_adr[-2]);
+}
+
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
